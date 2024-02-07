99350e7f4d036eca8ef93c705904d37ddb929fff
kritika1-21 <krihirsehgal@gmail.com>
2024-02-07 14:41:07 +0530

    this is message

---
+++ fir.java
@@ -0,0 +1,16 @@
+class student{
+   public static void main(String args[])
+   {
+      int pi=3000;
+      int rate=3;
+      int time=3;
+      int n=10;
+      int si=(pi*rate*time)/100;
+      int ci=pi(1+rate/n)^n*time;
+
+       System.out.println(si);
+       
+       System.out.println(ci);
+
+   }
+}
